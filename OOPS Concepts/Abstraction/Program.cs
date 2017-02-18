177334e5c7e7945d4b9402a0f08fef6989f58ea6
Ashwin Kumar <ashwinextrasall@gmail.com>
2017-02-18 19:51:54 +0530

    Working model

---
+++ OOPS Concepts/Abstraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Animal
+    {
+        public abstract void Eat();
+        public void Move()
+        {
+            Console.WriteLine("This is move method in abstract class");
+        }
+    }
+
+    public class Monkey : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("This is eat method from monkey class");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is main");
+            Console.ReadKey();
+        }
+    }
+}
