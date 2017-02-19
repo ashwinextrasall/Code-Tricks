2bb47c817498caca4dcdb4cf601fdc0599f4e367
Ashwin Kumar <ashwinextrasall@gmail.com>
2017-02-19 09:15:32 +0530

    Encapsulation Added

--- OOPS Concepts/Encapsulation/Program.cs
+++ OOPS Concepts/Encapsulation/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace Encapsulation
 {
+    class Rectangle
+    {
+        public double length;
+        public double breadth;
+
+        public double GetArea()
+        {
+            return length * breadth;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length : {0}",length);
+            Console.WriteLine("Width : {0}", breadth);
+            Console.WriteLine("Area : {0}",GetArea());
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Rectangle rectangleObj = new Rectangle();
+            rectangleObj.length = 4.5;
+            rectangleObj.breadth = 5.5;
+            rectangleObj.Display();
+            Console.ReadKey();
         }
     }
 }
