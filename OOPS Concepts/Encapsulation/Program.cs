65f5eb990b42df2e9ba566bd465ca39d11c37601
Ashwin Kumar <ashwinextrasall@gmail.com>
2017-02-19 09:25:57 +0530

    Encapsulation Added

--- OOPS Concepts/Encapsulation/Program.cs
+++ OOPS Concepts/Encapsulation/Program.cs
@@ -8,18 +8,35 @@ namespace Encapsulation
 {
     class Rectangle
     {
-        public double length;
-        public double breadth;
+        //Public Access Specifiers
+        //public double length;
+        //public double breadth;
+
+        //Private Access Specifiers
+        private double length;
+        private double breadth;
+
+        public double Getlength()
+        {
+            length = 4.5;
+            return length;
+        }
+
+        public double Getbreadth()
+        {
+            breadth= 5.5;
+            return breadth;
+        }
 
         public double GetArea()
-        {
-            return length * breadth;
+        { 
+            return Getlength() * Getbreadth();
         }
 
         public void Display()
         {
-            Console.WriteLine("Length : {0}",length);
-            Console.WriteLine("Width : {0}", breadth);
+            Console.WriteLine("Length : {0}", Getlength());
+            Console.WriteLine("Width : {0}", Getbreadth());
             Console.WriteLine("Area : {0}",GetArea());
         }
     }
@@ -28,8 +45,9 @@ namespace Encapsulation
         static void Main(string[] args)
         {
             Rectangle rectangleObj = new Rectangle();
-            rectangleObj.length = 4.5;
-            rectangleObj.breadth = 5.5;
+            //Commented to change from public to private access modifiers.
+            //rectangleObj.length = 4.5;
+            //rectangleObj.breadth = 5.5;
             rectangleObj.Display();
             Console.ReadKey();
         }
